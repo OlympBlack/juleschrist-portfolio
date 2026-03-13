322d68f99805e301a470c05de16a7320895d0fd7
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2026-03-13 12:53:49 +0100

    Added google analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,52 @@
-import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import ReactGA from "react-ga4";
 
-import { Layout } from './layout/Layout';
-import { Home } from './pages/Home';
-import { ProjectDetail } from './pages/ProjectDetail';
+import { Layout } from "./layout/Layout";
+import { Home } from "./pages/Home";
+import { ProjectDetail } from "./pages/ProjectDetail";
+
+ReactGA.initialize("G-CMTELEY02L");
+
+
+// Composant pour tracker les changements de page
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
+
   useEffect(() => {
     AOS.init({
       duration: 800,
       once: true,
       offset: 100,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   }, []);
 
   return (
     <Router>
+      <AnalyticsTracker />
+
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
         </Routes>
       </Layout>
+
     </Router>
   );
 }
